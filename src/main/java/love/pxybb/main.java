9257c72a7ade8c7c5ab444c6d05c4fecdf10f6c2
Tu-Muca <duan20090216@qq.com>
2026-01-17 01:44:12 +0800

    1.0

--- src/main/java/love/pxybb/main.java
+++ src/main/java/love/pxybb/main.java
@@ -1,10 +1,12 @@
 package love.pxybb;
 
+import love.pxybb.AreaCreate.ChestCreate.ChestCheck;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin {
     @Override
     public void onEnable() {
-
+        // 注册箱子检测监听器
+        getServer().getPluginManager().registerEvents(new ChestCheck(this), this);
     }
 }
